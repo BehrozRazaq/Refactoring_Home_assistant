c8cb9b1cac2327dee21cfecbd94a0b8724b7ece0
Simon Larsson <zico.xp.larsson@gmail.com>
2023-10-09 15:05:19 +0200

    Different implementation of string concat to reduce cog complexity. Created dummy test to verify functionality

--- homeassistant/components/tuya/sensor.py
+++ homeassistant/components/tuya/sensor.py
@@ -1118,9 +1118,8 @@ class TuyaSensorEntity(TuyaEntity, SensorEntity):
         """Init Tuya sensor."""
         super().__init__(device, device_manager)
         self.entity_description = description
-        self._attr_unique_id = (
-            f"{super().unique_id}{description.key}{description.subkey or ''}"
-        )
+
+        self._attr_unique_id = ''.join(filter(None, [super().unique_id, description.key, description.subkey]))
 
         if int_type := self.find_dpcode(description.key, dptype=DPType.INTEGER):
             self._type_data = int_type
---
+++ tests/components/tuya/test_string_concat.py
@@ -0,0 +1,40 @@
+import random
+import string
+import pytest
+
+
+# Functions to be compared in test ( new_string_concat(...) and old_string_concat(...) )
+def new_string_concat(str1, str2, str3):
+    result = ''.join(filter(None, [str1, str2, str3]))
+    return result
+
+
+def old_string_concat(str1, str2, str3):
+    result = f"{str1}{str2}{str3 or ''}"
+    return result
+
+
+# Generates random string of random length
+def generate_random_string(min_length, max_length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random_length = random.randint(min_length, max_length)
+    random_string = ''.join(random.choice(characters) for _ in range(random_length))
+    return random_string
+
+
+# Defines test cases to cover different inputs
+@pytest.mark.parametrize("input1, input2, input3", [
+    # Test case 1: only None
+    (None, None, None),
+    # Test case 2: only empty
+    ('', '', ''),
+    # Test case 3: only random
+    (generate_random_string(1, 15), generate_random_string(1, 15), generate_random_string(1, 15)),
+    # Test case 4: combination
+    (None, '', generate_random_string(1, 15)),
+])
+def test_string_concat_same(input1, input2, input3):
+    expected_string = old_string_concat(input1, input2, input3)
+    actual_string = new_string_concat(input1, input2, input3)
+
+    assert actual_string == expected_string
