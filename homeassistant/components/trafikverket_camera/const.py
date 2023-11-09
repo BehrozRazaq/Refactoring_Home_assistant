d4a85c319530b9e687ea1d8db9fd6a4ad182e596
frixxo <joel.olaus@gmail.com>
2023-11-09 13:58:18 +0100

    Added traffic measure
    added traffic measure based on mock statistics.
    
    Co-Authored-By: Erik Eriksson <37438311+OneZeroWasTaken@users.noreply.github.com>

--- homeassistant/components/trafikverket_camera/const.py
+++ homeassistant/components/trafikverket_camera/const.py
@@ -1,4 +1,5 @@
 """Adds constants for Trafikverket Camera integration."""
+from enum import Enum
 from homeassistant.const import Platform
 
 DOMAIN = "trafikverket_camera"
@@ -8,3 +9,12 @@ ATTRIBUTION = "Data provided by Trafikverket"
 
 ATTR_DESCRIPTION = "description"
 ATTR_TYPE = "type"
+
+
+class TrafficMeasure(Enum):
+    Unknown = 0
+    Low = 1
+    Medium = 2
+    High = 3
+    Critical = 4
+
