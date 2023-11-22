f8ce220f260606733f4db82a1ee864f99e82b7f9
s33b00r <qubert00@live.se>
2023-11-22 17:58:16 +0000

    Connect database, backend, frontend, and model

--- homeassistant/components/trafikverket_camera/statistics.py
+++ homeassistant/components/trafikverket_camera/statistics.py
@@ -1,15 +1,24 @@
+"""Handles all the statistics."""
 from .traffic_data_operations import Operations
 
 
 class StatisticsHandler:
+    """Class where data is handled for the statistics."""
+
     statistics: list[tuple[str, int]]
 
-    def __init__(self, location):
-        self.statistics = Operations.query_time_and_cars_by_location(location)
+    def __init__(self, location: str) -> None:
+        """Location -> which location is used for the trafikverket camera."""
+        self.database_connector = Operations()
+        self.statistics = self.database_connector.query_time_and_cars_by_location(
+            location
+        )
 
-    def new_entry(self, location: str, time: str, nr_cars: int):
+    def new_entry(self, location: str, time: str, nr_cars: int) -> None:
+        """Save a new data point."""
         self.statistics.append((time, nr_cars))
-        Operations.insert_traffic_entry(location, time, nr_cars)
+        self.database_connector.insert_traffic_entry(location, time, nr_cars)
 
     def get_data(self) -> list[tuple[str, int]]:
+        """Retrieve all statistics data for a location."""
         return self.statistics
