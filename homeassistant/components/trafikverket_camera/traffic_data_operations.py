be0e6ab86c0b40e51ea40b613f871cf3743bd398
Oscar Forsyth <oscar.forsyth61@gmail.com>
2023-12-06 14:25:02 +0000

    small changes

--- homeassistant/components/trafikverket_camera/traffic_data_operations.py
+++ homeassistant/components/trafikverket_camera/traffic_data_operations.py
@@ -3,8 +3,6 @@ import os
 import sqlite3
 from typing import Optional
 
-from .const import DATABASE
-
 
 class Operations:
     """Database interface."""
@@ -12,15 +10,20 @@ class Operations:
     def __init__(self, config_dir: str) -> None:
         """Create an empty table if traffic_amount is not present."""
         self.database_path = os.path.join(config_dir, "home-assistant_v2.db")
-        with sqlite3.connect(self.database_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                "SELECT name FROM sqlite_master WHERE type='table' AND name='traffic_amount'"
-            )
-            if cursor.fetchone() is None:
+        try:
+            with sqlite3.connect(self.database_path) as conn:
+                cursor = conn.cursor()
                 cursor.execute(
-                    "CREATE TABLE traffic_amount (location VARCHAR(255), time DATETIME, nr_cars INTEGER NOT NULL)"
+                    "SELECT name FROM sqlite_master WHERE type='table' AND name='traffic_amount'"
                 )
+                if cursor.fetchone() is None:
+                    cursor.execute(
+                        "CREATE TABLE traffic_amount (location VARCHAR(255), time DATETIME, nr_cars INTEGER NOT NULL)"
+                    )
+                    cursor.commit()
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
+            
 
     def insert_traffic_entry(self, location: str, time: str, nr_cars: int) -> None:
         """Insert a new traffic entry into the database.
@@ -30,14 +33,17 @@ class Operations:
             time: The timestamp of the traffic entry.
             nr_cars: The number of cars at the given location and time.
         """
-        with sqlite3.connect(self.database_path) as conn:
-            conn.cursor()
-            cursor = conn.cursor()
-            cursor.execute(
-                "INSERT INTO traffic_amount (location, time, nr_cars) VALUES (?,?,?)",
-                (location, time, nr_cars),
-            )
-            conn.commit()
+        try:
+            with sqlite3.connect(self.database_path) as conn:
+                conn.cursor()
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO traffic_amount (location, time, nr_cars) VALUES (?,?,?)",
+                    (location, time, nr_cars),
+                )
+                conn.commit()
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
 
     def query_time_and_cars_by_location(self, location: str) -> list[tuple[str, int]]:
         """Query the time and number of cars for a specific location.
@@ -48,16 +54,19 @@ class Operations:
         Returns:
             A list of tuples containing the time and number of cars at the specified location.
         """
-        with sqlite3.connect(self.database_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                "SELECT time, nr_cars FROM traffic_amount WHERE location = ?",
-                (location,),
-            )
-            entries = cursor.fetchall()
-        if not entries:
-            return []
-        return entries
+        try:
+            with sqlite3.connect(self.database_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "SELECT time, nr_cars FROM traffic_amount WHERE location = ?",
+                    (location,),
+                )
+                entries = cursor.fetchall()
+            if not entries:
+                return []
+            return entries
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
 
     def query_time_and_cars_by_location_and_time(
         self, location: str, start_time: str, end_time: str
@@ -72,15 +81,18 @@ class Operations:
         Returns:
         A list of tuples containing the time and number of cars at the specified location and time span.
         """
-        with sqlite3.connect(self.database_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                """
-                SELECT time, nr_cars
-                FROM traffic_amount
-                WHERE location = ? AND time >= ? AND time <= ?
-                """,
-                (location, start_time, end_time),
-            )
-            data = cursor.fetchall()
+        try:
+            with sqlite3.connect(self.database_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    """
+                    SELECT time, nr_cars
+                    FROM traffic_amount
+                    WHERE location = ? AND time >= ? AND time <= ?
+                    """,
+                    (location, start_time, end_time),
+                )
+                data = cursor.fetchall()
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
         return data
