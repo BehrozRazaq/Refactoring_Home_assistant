7ef06e4c79aa7c15a13292b71b837b485c69b922
s33b00r <qubert00@live.se>
2023-12-11 22:53:42 +0000

    Add ability to change timespan for statistics.

--- config/www/trafikverket-statistics-view.js
+++ config/www/trafikverket-statistics-view.js
@@ -10,26 +10,92 @@ export class StatisticsView extends LitElement {
   static properties = {
     data: [{ location: String, time: String, nr_cars: Number }],
     name: String,
+    display: String,
+    chart: {},
   };
 
-  firstUpdated() {
-    //this.hass.states[this.config.cameras[this.selectedIndex]];
-    //nrOfCars = cameraData.attributes.
+  constructor() {
+    super();
+    this.display = "day";
+  }
+
+  updated(updatedItems) {
+    const namesOfUpdated = Array.from(updatedItems.keys());
+    if (namesOfUpdated.length == 1 && namesOfUpdated[0] == "chart") {
+      return;
+    }
+    if (this.chart) {
+      this.chart.destroy();
+      this.chart = undefined;
+    }
     const ctx = this.shadowRoot.getElementById("statistics").getContext("2d");
-    const refinedData = JSON.parse(this.data).map((d) => ({
+    let refinedData = JSON.parse(this.data).map((d) => ({
       x: d[0],
       y: d[1],
     }));
 
-    //const labels = utils.months;
+    const timeFrames = {
+      hour: 3600 * 1000,
+      day: 3600 * 1000 * 24,
+      week: 3600 * 1000 * 24 * 7,
+      month: 3600 * 1000 * 24 * 30,
+      year: 3600 * 1000 * 24 * 365,
+    };
 
-    new Chart(ctx, {
+    const units = {
+      hour: "minute",
+      day: "hour",
+      week: "day",
+      month: "day",
+      year: "day",
+    };
+
+    const now = new Date();
+    const oldest = new Date(now.getTime() - timeFrames[this.display]);
+    refinedData = refinedData.filter((d) => {
+      const dataTime = new Date(d.x);
+      return dataTime > oldest;
+    });
+
+    refinedData = refinedData.reduce((acc, d) => {
+      let date = new Date(d.x);
+      switch (this.display) {
+        case "hour":
+          break;
+        case "day":
+          date.setMinutes(Math.floor(date.getMinutes() / 30) * 30, 0, 0);
+          break;
+        case "week":
+          date.setHours(Math.floor(date.getHours() / 4) * 4, 0, 0, 0);
+          break;
+        case "month":
+        case "year":
+          date.setHours(0, 0, 0, 0);
+          break;
+      }
+      let time = date.toISOString();
+      if (!acc[time]) {
+        acc[time] = [];
+      }
+      acc[time].push(d.y);
+      return acc;
+    }, {});
+
+    let data = [];
+    for (let key in refinedData) {
+      let sum = refinedData[key].reduce((a, b) => a + b, 0);
+      data.push({ x: key, y: Math.round(sum / refinedData[key].length) });
+    }
+
+    console.log(data);
+
+    this.chart = new Chart(ctx, {
       type: "line",
       data: {
         datasets: [
           {
             label: "Number of Cars",
-            data: refinedData,
+            data: data,
             borderWidth: 1,
           },
         ],
@@ -44,7 +110,7 @@ export class StatisticsView extends LitElement {
             type: "time",
             time: {
               //This unit can be changed to match what we want
-              unit: "hour",
+              unit: units[this.display],
             },
           },
         },
@@ -68,6 +134,43 @@ export class StatisticsView extends LitElement {
   render() {
     return html` <div id="statistics-container">
       <h3 id="statistics-title">${this.name}</h3>
+      <div id="statistics-buttons">
+        <button
+          @click="${(_) => {
+            this.display = "hour";
+          }}"
+        >
+          Hour
+        </button>
+        <button
+          @click="${(_) => {
+            this.display = "day";
+          }}"
+        >
+          Day
+        </button>
+        <button
+          @click="${(_) => {
+            this.display = "week";
+          }}"
+        >
+          Week
+        </button>
+        <button
+          @click="${(_) => {
+            this.display = "month";
+          }}"
+        >
+          Month
+        </button>
+        <button
+          @click="${(_) => {
+            this.display = "year";
+          }}"
+        >
+          Year
+        </button>
+      </div>
       <canvas id="statistics"></canvas>
     </div>`;
   }
